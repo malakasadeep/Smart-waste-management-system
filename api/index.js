27b7977f0f43581809d15a9bb317fde4622b927d
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-10-16 18:20:33 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.listen(3000, () => {
+  console.log("Server listening on port 3000!!!");
+});
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to Mongo DB successfully!!!");
+  })
+  .catch((err) => {
+    console.log("Error connecting to Mongo");
+  });
